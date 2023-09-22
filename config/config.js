8b00e8f66f79e5c06f3aaa19cd8051f3b67897db
seun awonugba <86803598+seunAwonugba@users.noreply.github.com>
2023-09-23 00:47:29 +0100

    implement db setup

---
+++ config/config.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+module.exports = {
+    development: {
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DEV_DATABASE,
+        host: process.env.DB_HOST,
+        dialect: process.env.DB_DIALECT,
+    },
+    test: {
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.TEST_DATABASE,
+        host: process.env.DB_HOST,
+        dialect: process.env.DB_DIALECT,
+    },
+    production: {
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.PROD_DATABASE,
+        host: process.env.DB_HOST,
+        dialect: process.env.DB_DIALECT,
+    },
+};
