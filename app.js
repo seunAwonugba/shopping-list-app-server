812305d994bbd522b096bee98b50255feea68b69
seun awonugba <86803598+seunAwonugba@users.noreply.github.com>
2023-09-28 13:32:01 +0100

    update user sign up

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const { StatusCodes, ReasonPhrases } = require("http-status-codes");
 const { sequelize } = require("./models/index");
 const user = require("./routers/user");
 const item = require("./routers/item");
+const { errorMiddleware } = require("./middleware/errorMiddleware");
 
 const app = express();
 
@@ -15,30 +16,39 @@ app.use("/api/v1/user", user);
 app.use("/api/v1/items", item);
 
 app.get("/api/v1", (req, res) => {
-  res.status(StatusCodes.OK).json({
-    success: true,
-    data: "Home page",
-  });
+    res.status(StatusCodes.OK).json({
+        success: true,
+        data: "Home page",
+    });
 });
 
 app.all("*", (req, res) => {
-  res.status(StatusCodes.NOT_FOUND).json({
-    success: false,
-    data: ReasonPhrases.NOT_FOUND,
-  });
+    res.status(StatusCodes.NOT_FOUND).json({
+        success: false,
+        data: ReasonPhrases.NOT_FOUND,
+    });
 });
 
-const startServer = async () => {
-  try {
-    await sequelize.authenticate();
-    console.log(`⚡️[database]: Connection has been established successfully.`);
+app.use(errorMiddleware);
 
-    app.listen(port, host, () => {
-      console.log(`⚡️[server]: Server is running at http://${host}:${port}`);
-    });
-  } catch (error) {
-    console.error("😔[database]: Unable to connect to the database:", error);
-  }
+const startServer = async () => {
+    try {
+        await sequelize.authenticate();
+        console.log(
+            `⚡️[database]: Connection has been established successfully.`
+        );
+
+        app.listen(port, host, () => {
+            console.log(
+                `⚡️[server]: Server is running at http://${host}:${port}`
+            );
+        });
+    } catch (error) {
+        console.error(
+            "😔[database]: Unable to connect to the database:",
+            error
+        );
+    }
 };
 
 startServer();
