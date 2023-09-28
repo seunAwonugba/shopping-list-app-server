3df653530e729d6ace5642b0d883f7623b92a2e8
seun awonugba <86803598+seunAwonugba@users.noreply.github.com>
2023-09-28 13:43:55 +0100

    update items model

--- migrations/20230926163517-create-item.js
+++ migrations/20230926163517-create-item.js
@@ -1,41 +1,53 @@
 "use strict";
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable("items", {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER,
-      },
-      name: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      quantity: {
-        type: Sequelize.STRING,
-      },
-      category: {
-        type: Sequelize.STRING,
-      },
-      price: {
-        type: Sequelize.STRING,
-      },
-      completed: {
-        type: Sequelize.BOOLEAN,
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-    });
-  },
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable("items");
-  },
+    async up(queryInterface, Sequelize) {
+        await queryInterface.createTable("items", {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER,
+            },
+            name: {
+                type: Sequelize.STRING,
+                allowNull: false,
+                validate: {
+                    notEmpty: {
+                        msg: "First Name is required",
+                    },
+                    notNull: {
+                        msg: "First Name can not be null",
+                    },
+                },
+            },
+            quantity: {
+                type: Sequelize.STRING,
+                allowNull: false,
+                validate: {
+                    notEmpty: {
+                        msg: "Last Name is required",
+                    },
+                    notNull: {
+                        msg: "Last Name can not be null",
+                    },
+                },
+            },
+            notes: {
+                type: Sequelize.STRING,
+                allowNull: true,
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+        });
+    },
+    async down(queryInterface, Sequelize) {
+        await queryInterface.dropTable("items");
+    },
 };
