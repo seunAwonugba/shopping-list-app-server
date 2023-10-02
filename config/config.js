119135bbab9a7f94125259157c479bc91013c5ba
isiagi <isiagigeofrey0@gmail.com>
2023-10-02 09:58:19 +0300

    chore: fix render error

--- config/config.js
+++ config/config.js
@@ -1,24 +1,30 @@
 require("dotenv").config();
 module.exports = {
-    development: {
-        username: process.env.DB_USERNAME,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DEV_DATABASE,
-        host: process.env.DB_HOST,
-        dialect: process.env.DB_DIALECT,
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DEV_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+  },
+  test: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.TEST_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+  },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.PROD_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
     },
-    test: {
-        username: process.env.DB_USERNAME,
-        password: process.env.DB_PASSWORD,
-        database: process.env.TEST_DATABASE,
-        host: process.env.DB_HOST,
-        dialect: process.env.DB_DIALECT,
-    },
-    production: {
-        username: process.env.DB_USERNAME,
-        password: process.env.DB_PASSWORD,
-        database: process.env.PROD_DATABASE,
-        host: process.env.DB_HOST,
-        dialect: process.env.DB_DIALECT,
-    },
+  },
 };
